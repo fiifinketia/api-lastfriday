ed43e7edb3e15e7f553170aa7dbb31398cc162c6
RMQ <mcquaye@hotmail.com>
2024-08-28 05:50:05 +0000

    web route removed

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CloudinaryController;
 
 
-Route::get('/gallery/{imageName}', [CloudinaryController::class, 'show']);
+// Route::get('/gallery/{imageName}', [CloudinaryController::class, 'show']);
